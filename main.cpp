9dc90ccd2e6e54dfa6d71e26cda1825ffce93424
newbility <newbility523@gmail.com>
2020-01-19 08:57:23 +0800

    update

--- main.cpp
+++ main.cpp
@@ -5,23 +5,62 @@ using namespace std;
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int l = 0;
-        int r = 0;
-        int maxSame = 0;
+        int startL = 0;
+        int startR = 0;
+
+        int maxLenght = 1;
+        int resultL = startL;
         int length = s.length();
-        while (l < length)
+
+        int saveCountMax = 100000;
+        int count = 0;
+        while (startL < length)
         {
-            if (l - 1 > 0 && r + 1 < length)
+            int l = startL;
+            int r = startR;
+            bool hasSame = false;
+            while (s[l] == s[r])
             {
-                if (s[l - 1] == s[r + 1])
+                hasSame = true;
+                if (l - 1 >= 0 && r + 1 < length)
                 {
                     l -= 1;
                     r += 1;
                 }
+                else
+                {
+                    break;
+                }
+            }
+
+            if (hasSame)
+            {
+                int curLength = r - l + 1;
+                if (curLength > maxLenght)
+                {
+                    maxLenght = curLength;
+                    resultL = l;
+                }
+            }
+
+            if (startL == startR)
+            {
+                startR += 1;
+            }
+            else
+            {
+                startL += 1;
+            }
+
+            count += 1;
+            if (count > saveCountMax)
+            {
+                std::cout << "dead loop maybe !" << std::endl;
+                break;
             }
         }
 
-        string result = s.substr(l, r - l + 1);
+        string result = s.substr(resultL, maxLenght);
         return result;
     }
 };
@@ -32,10 +71,11 @@ int main()
     //{
     //    std::cout << "hello world" << std::endl;
     //}
-    string s = "aabbccefg";
+    string s = "aaccbbccefg";
     Solution solution = Solution();
     string result = solution.longestPalindrome(s);
     std::cout << result << std::endl;
 
+    //std::cout << "hello world" << std::endl;
     return 0;
 }
